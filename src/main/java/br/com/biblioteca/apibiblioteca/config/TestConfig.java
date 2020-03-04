ca03a9b3ac72736d8ac2e62ee577aecb40c9e738
Waldir Sousa <waldir.sousa@phoebus.local>
2020-03-04 18:32:32 -0300

    implementation of the book repository

---
+++ src/main/java/br/com/biblioteca/apibiblioteca/config/TestConfig.java
@@ -0,0 +1,4 @@
+package br.com.biblioteca.apibiblioteca.config;
+
+public class TestConfig {
+}
---
+++ src/main/java/br/com/biblioteca/apibiblioteca/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package br.com.biblioteca.apibiblioteca.repository;
+
+import br.com.biblioteca.apibiblioteca.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
