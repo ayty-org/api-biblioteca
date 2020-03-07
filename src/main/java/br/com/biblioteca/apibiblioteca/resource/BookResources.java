0d5088788a41da071cb895f1675602f713ab1e45
waldir.sousa <waldir.sousa@phoebustecnologia.com.br>
2020-03-07 10:16:01 -0300

    fixing Get paginated book bug

--- src/main/java/br/com/biblioteca/apibiblioteca/resource/BookResources.java
+++ src/main/java/br/com/biblioteca/apibiblioteca/resource/BookResources.java
@@ -37,7 +37,7 @@ public class BookResources {
     public ResponseEntity<Page<Book>> findPage(
             @RequestParam(value="page", defaultValue="0") Integer page,
             @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
-            @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+            @RequestParam(value="orderBy", defaultValue="title") String orderBy,
             @RequestParam(value="direction", defaultValue="ASC") String direction){
         Page<Book> list = service.findPage(page, linesPerPage, orderBy, direction);
         return ResponseEntity.ok().body(list);
