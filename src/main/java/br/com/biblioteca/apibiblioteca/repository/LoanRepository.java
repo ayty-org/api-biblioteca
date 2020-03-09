5f9741173f88d796d4e88a789a3c5a1b14026473
waldir.sousa <waldir.sousa@phoebustecnologia.com.br>
2020-03-09 15:52:24 -0300

    resources controller implementation for loans

--- src/main/java/br/com/biblioteca/apibiblioteca/repository/LoanRepository.java
+++ src/main/java/br/com/biblioteca/apibiblioteca/repository/LoanRepository.java
@@ -1,4 +1,9 @@
 package br.com.biblioteca.apibiblioteca.repository;
 
-public class LoanRepository {
+import br.com.biblioteca.apibiblioteca.domain.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
