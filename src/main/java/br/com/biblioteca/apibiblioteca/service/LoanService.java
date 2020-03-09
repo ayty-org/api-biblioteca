af17c31d754ef130bd705ca6ed44289b0a8165d9
waldir.sousa <waldir.sousa@phoebustecnologia.com.br>
2020-03-09 15:56:33 -0300

    implementation service for loans

--- src/main/java/br/com/biblioteca/apibiblioteca/service/LoanService.java
+++ src/main/java/br/com/biblioteca/apibiblioteca/service/LoanService.java
@@ -1,4 +1,56 @@
 package br.com.biblioteca.apibiblioteca.service;
 
+import br.com.biblioteca.apibiblioteca.domain.Book;
+import br.com.biblioteca.apibiblioteca.domain.Loan;
+import br.com.biblioteca.apibiblioteca.repository.LoanRepository;
+import br.com.biblioteca.apibiblioteca.service.exception.DataIntegrityException;
+import br.com.biblioteca.apibiblioteca.service.exception.ObjectNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LoanService {
+
+    private LoanRepository repo;
+
+    public Loan find (Long id) throws ObjectNotFoundException {
+        Optional<Loan> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(      //estudar como funciona esse retorno
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Loan.class.getName()));
+    }
+
+    public Loan insert(Loan obj){ //Insere um livro no banco
+        return repo.save(obj);
+    }
+
+    public Loan update (Loan obj){
+        Book newObj = find(obj.getId());
+        return repo.save(obj);
+    }
+
+    public void delete(Long id){
+        find(id);
+        try {
+            repo.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possivevel excluir uma User que possui dependências");
+        }
+    }
+
+    public List<Loan> findAll() {
+        return repo.findAll();
+    }
+
+    public Page<Loan> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage , Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
 }
