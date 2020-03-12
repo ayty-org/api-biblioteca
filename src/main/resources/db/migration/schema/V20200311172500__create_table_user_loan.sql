2bfb57fd35c1f60b926346a3d9b105475c9652e9
Waldir Marques <waldir.marques@dcx.ufpb.br>
2020-03-12 14:16:57 -0300

    modification in sql migration

--- src/main/resources/db/migration/schema/V20200311172500__create_table_user_loan.sql
+++ src/main/resources/db/migration/schema/V20200311172500__create_table_user_loan.sql
@@ -1,10 +1,10 @@
-CREATE TABLE public.user_loan (
-	user_app_id int8 NOT NULL,
-loan_id int8 NOT NULL
-);
+--CREATE TABLE user_loan (
+--	user_app_id int8 NOT NULL,
+ --   loan_id int8 NOT NULL
+--);
 
 
 -- public.user_loan foreign keys
 
-ALTER TABLE user_loan ADD CONSTRAINT fk2smxk6cl6w7e4cflbllnnep8o FOREIGN KEY (user_app_id) REFERENCES user_app(id);
-ALTER TABLE user_loan ADD CONSTRAINT fkaor37c4wjm37v84huo5ymwusd FOREIGN KEY (loan_id) REFERENCES loan(id);
+--ALTER TABLE user_loan ADD CONSTRAINT fk2smxk6cl6w7e4cflbllnnep8o FOREIGN KEY (user_app_id) REFERENCES user_app(id);
+--ALTER TABLE user_loan ADD CONSTRAINT fkaor37c4wjm37v84huo5ymwusd FOREIGN KEY (loan_id) REFERENCES loan(id);
