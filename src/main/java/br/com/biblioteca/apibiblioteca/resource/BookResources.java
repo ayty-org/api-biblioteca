a6eadf79df742c0909d94a931af854299d282d28
waldir.sousa <waldir.sousa@phoebustecnologia.com.br>
2020-03-21 17:39:23 -0300

    implementation of dto to book entety

--- src/main/java/br/com/biblioteca/apibiblioteca/resource/BookResources.java
+++ src/main/java/br/com/biblioteca/apibiblioteca/resource/BookResources.java
@@ -1,6 +1,7 @@
 package br.com.biblioteca.apibiblioteca.resource;
 
 import br.com.biblioteca.apibiblioteca.domain.Book;
+import br.com.biblioteca.apibiblioteca.dto.BookDTO;
 import br.com.biblioteca.apibiblioteca.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/v1/api/book")
@@ -19,9 +21,10 @@ public class BookResources {
     private BookService bookService;
 
     @GetMapping //lista todos os livros
-    public ResponseEntity<List<Book>> findAll() {
+    public ResponseEntity<List<BookDTO>> findAll() {
         List<Book> list = bookService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<BookDTO> listDto = list.stream().map(obj -> new BookDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
     @GetMapping(value="/{id}") //lista livros por id
@@ -31,26 +34,29 @@ public class BookResources {
     }
 
     @GetMapping(value = "/page") //lista todas os livros com paginação
-    public ResponseEntity<Page<Book>> findPage(
+    public ResponseEntity<Page<BookDTO>> findPage(
             @RequestParam(value="page", defaultValue="0") Integer page,
             @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
             @RequestParam(value="orderBy", defaultValue="title") String orderBy,
             @RequestParam(value="direction", defaultValue="ASC") String direction){
         Page<Book> list = bookService.findPage(page, linesPerPage, orderBy, direction);
-        return ResponseEntity.ok().body(list);
+        Page<BookDTO> listDTO = list.map(obj -> new BookDTO(obj));
+        return ResponseEntity.ok().body(listDTO);
     }
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping //adiciona um novo Book
-    public void insert(@Valid @RequestBody Book obj){
-        bookService.insert(obj);
+    public void insert(@Valid @RequestBody BookDTO objDto){
+        Book book = bookService.fromDTO(objDto);
+        bookService.insert(book);
     }
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @PutMapping(value="/{id}") //atualizar uma Book
-    public void update(@Valid @RequestBody Book obj, @PathVariable Long id){
-        obj.setId(id);
-        bookService.update(obj);
+    public void update(@Valid @RequestBody BookDTO objDto, @PathVariable Long id){
+        Book book = bookService.fromDTO(objDto);
+        book.setId(id);
+        bookService.update(book);
     }
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
